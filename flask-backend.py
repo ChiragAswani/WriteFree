a113397219e9b69ec7fcab2d2145df0013421b44
zsqiii <zhangspp199267@gmail.com>
2018-10-07 19:58:39 -0400

    Updates
    1. Backend is able to receive request for creating usrname+pwd under POST method at the URL /create-account
    2. Going to the url: /retrieve will list the titles for all document in the credential collection .
    3.  Built a basic MongoEngine document structure.

--- flask-backend.py
+++ flask-backend.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_mongoengine import MongoEngine
-
+from mongoengine import *
 '''
 Date: Oct 7th 2018
 Descrption: This is the file for the flask backend
@@ -10,24 +10,47 @@ app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
     'db': 'WriteFreeDB',
-    'host': 'localhost',
+
+    'host': '127.0.0.1',
     'port': 27017
 }
 db = MongoEngine(app)
 #global counter
 counter = 0
 
+# stored class
+class credentials(Document):
+    name = StringField(required=True, max_length=15)
+    pwd = StringField(required=True, max_length=12)
+
+# post definition
+class Post(Document):
+    title = StringField(max_length=120, required=True)
+    author = ReferenceField(credentials)
+    # allow for inheritance
+    meta = {'allow_inheritance': True}
+
 @app.route('/')
 def hello():
-    return 'hello!'
+    global counter
+    return str(counter)
 
 # create account and store info into DB
-@app.route('/create-account', methods= ['GET'])
+@app.route('/create-account', methods= ['POST'])
 def create():
-    
-    return 'Account added'
+    global counter
+    counter += 1
+    if request.method == 'POST':
+        usrname = request.args['username']
+        pwd = request.args['password']
+    usr = credentials(name=usrname, pwd=pwd).save()
+    post = Post(title=usrname, author=usr)
+    post.save()
+    return 'Account ' + str(counter) + ' added'
 
 # retrieve the account info and display it to the web
 @app.route ('/retrieve')
 def retrieve():
-    return 'retrieve'
+    for post in Post.objects:
+        print(post.title)
+    return 'Done retrieving'
