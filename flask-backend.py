b3118bf0a78d6ce945d4d0f17d10ac97cd753a9e
zsqiii <zhangspp199267@gmail.com>
2018-10-07 18:30:27 -0400

    Updates

--- flask-backend.py
+++ flask-backend.py
@@ -1,10 +1,33 @@
-from flask import Flask
-from numpy import np
+from flask import Flask, request
 from flask_mongoengine import MongoEngine
+
 '''
 Date: Oct 7th 2018
 Descrption: This is the file for the flask backend
 '''
+# initializations
 app = Flask(__name__)
-app.config.from_pyfile('the-config.cfg')
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'WriteFreeDB',
+    'host': 'localhost',
+    'port': 27017
+}
 db = MongoEngine(app)
+#global counter
+counter = 0
+
+@app.route('/')
+def hello():
+    return 'hello!'
+
+# create account and store info into DB
+@app.route('/create-account', methods= ['GET'])
+def create():
+    
+    return 'Account added'
+
+# retrieve the account info and display it to the web
+@app.route ('/retrieve')
+def retrieve():
+    return 'retrieve'
