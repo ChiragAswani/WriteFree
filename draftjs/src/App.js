b7fd95232cabfe858808de3c117368fa67d00de4
Chirag Aswani <chirag@aswani.net>
2018-10-07 15:32:13 -0400

    draftjs

--- draftjs/src/App.js
+++ draftjs/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, convertFromHTML, ContentState} from 'draft-js';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {editorState: EditorState.createEmpty()};
+        const sampleMarkup =
+    '<b>Chirag</b>, <i>Italic text</i><br/ ><br />' +
+    '<a href="http://www.facebook.com">Example link</a>';
+
+  const blocksFromHTML = convertFromHTML(sampleMarkup);
+  const state = ContentState.createFromBlockArray(
+    blocksFromHTML.contentBlocks,
+    blocksFromHTML.entityMap
+);
+        this.state = {editorState: EditorState.createWithContent(state)};
         this.onChange = (editorState) => this.setState({editorState});
+    }
+    method(){
+
     }
     render() {
         console.log(this.state.editorState)
