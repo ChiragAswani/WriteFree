5fdd98411e7b064b9b1c4f81407cc4f5f2059a0a
Chirag Aswani <chirag@aswani.net>
2018-10-25 17:37:05 -0400

    delete working

--- app/app.py
+++ app/app.py
@@ -40,8 +40,10 @@ def create():
             "defaultNoteSettings": {},
         }
         credentials_collection.insert_one(savedDocument)
-        del savedDocument['_id']
-        return ((json.dumps(savedDocument)), 200)
+        savedDocument["_id"] = str(savedDocument["_id"])
+        document = jsonify({"notes": [], "credentials": savedDocument})
+        print(document)
+        return (document, 200)
 
 # verify username and password, returns account details and notes
 @app.route('/login', methods= ['GET', 'OPTIONS'])
@@ -49,23 +51,27 @@ def login():
     email = request.args['email']
     password = request.args['password']
     credentials = credentials_collection.find_one({'email': email, 'password': password})
-    credentials["_id"] = str(credentials["_id"])
+    print(credentials)
     if (credentials):
-        userNotes = notes_collection.find({'email': email})
-        arrayOfNotes = []
-        for doc in userNotes:
-            doc["_id"] = str(doc["_id"])
-            arrayOfNotes.append(doc)
-        print(arrayOfNotes)
-        return jsonify({"noteData": arrayOfNotes, "credentials": credentials}), 200
+        credentials["_id"] = str(credentials["_id"])
+        arrayOfNotes = getArrayOfNotes(email)
+        return jsonify({"notes": arrayOfNotes, "credentials": credentials}), 200
     return "Invalid Username or Password", 401
+
+@app.route('/get-notes', methods= ['GET', 'OPTIONS'])
+def getNotes():
+    email = request.args['email']
+    arrayOfNotes = getArrayOfNotes(email)
+    return jsonify({"notes": arrayOfNotes}), 200
+
 
 @app.route ('/delete-note', methods= ['DELETE', 'OPTIONS'])
 def deleteNote():
     email = request.args['email']
     noteID = request.args['noteID']
     notes_collection.delete_one({'email': email, "_id": ObjectId(noteID)})
-    return "Successfull Deleted", 200
+    arrayOfNotes = getArrayOfNotes(email)
+    return jsonify({"notes": arrayOfNotes}), 200
 
 
 @app.route ('/new-note', methods= ['POST', 'OPTIONS'])
@@ -81,7 +87,7 @@ def addNote():
 
     }
     _id = notes_collection.insert(baseNewNote)
-    x = (notes_collection.find_one({"_id": ObjectId(_id)}))
+    x = notes_collection.find_one({"_id": ObjectId(_id)})
     x["_id"] = str(x["_id"])
     return jsonify(x), 200
 
@@ -93,6 +99,17 @@ def saveNote():
     notes_collection.update_one(query, {"$set": new_values})
     return "HI", 200
 
+@app.route ('/update-default-settings', methods= ['POST', 'OPTIONS'])
+def updateDefaultSettings():
+    form_data = json.loads(request.get_data())
+    email = form_data['email']
+    noteColor = form_data['noteColor']
+    applicationColor = form_data['applicationColor']
+    font = form_data['font']
+    query = {'$set': {'defaultNoteSettings': {'noteColor': noteColor, 'applicationColor': applicationColor, 'font': font}}}
+    credentials_collection.find_one_and_update({'email': email}, query)
+    return "HI", 200
+
 @app.route ('/fetch-note', methods= ['GET', 'OPTIONS'])
 def fetchNote():
     email = request.args['email']
@@ -101,3 +118,13 @@ def fetchNote():
     data = notes_collection.find_one({'email': email, "_id": ObjectId(noteID)})
     data["_id"] = str(data["_id"])
     return jsonify(data), 200
+
+
+
+def getArrayOfNotes(email):
+    userNotes = notes_collection.find({'email': email})
+    arrayOfNotes = []
+    for doc in userNotes:
+        doc["_id"] = str(doc["_id"])
+        arrayOfNotes.append(doc)
+    return arrayOfNotes
