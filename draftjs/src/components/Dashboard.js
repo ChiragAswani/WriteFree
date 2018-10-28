c77281e99b5472b2c7231fc93b4f54937c94d049
zsqiii <zhangspp199267@gmail.com>
2018-10-28 17:30:37 -0400

    Warning fixed
    Fixed the table row key warning by addidng the key-value pair to the note[i]:
    "key" : "_id"

--- draftjs/src/components/Dashboard.js
+++ draftjs/src/components/Dashboard.js
@@ -136,6 +136,12 @@ class Dashboard extends React.Component {
         //console.log("STATE", this.state)
         //console.log("HISTORY", this.props.location.state)
         if(this.validate()) {
+            var data =[];
+            //convert the userDate from the login page to the dateSource so that it can be used in Table opeartions
+            for (var i = 0; i < this.props.location.state.notes.length; i++){
+
+                this.props.location.state.notes[i]["key"] = this.props.location.state.notes[i]["_id"];
+            }
             return (
                 <div>
                     <Button type="primary"
