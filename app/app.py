42940295e23f472a4ada24fc32bd97873cd66ff9
zsqiii <zhangspp199267@gmail.com>
2018-11-29 17:37:22 -0500

    updates

--- app/app.py
+++ app/app.py
@@ -1,43 +1,48 @@
 from flask import Flask, request, jsonify, session, render_template, make_response, send_file
 import time
 from flask_cors import CORS
+from flask import Flask, request, jsonify, session, render_template, make_response, send_file
+import time
+from flask_cors import CORS
 from pymongo import MongoClient
 import json
 from bson.objectid import ObjectId
 from flask_bcrypt import Bcrypt
 import pdfkit
 import datetime
-from draftjs_exporter.constants import BLOCK_TYPES, ENTITY_TYPES
-from draftjs_exporter.defaults import BLOCK_MAP, STYLE_MAP
-from draftjs_exporter.dom import DOM
 from draftjs_exporter.html import HTML
 
 import MongoDBCalls as dbcalls
 import control as control
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token, create_refresh_token,
+    get_jwt_identity, jwt_refresh_token_required, get_raw_jwt
+)
 # initializations
 db_address = 'mongodb://localhost:27017/'
 app = Flask(__name__)
 CORS(app)
 app.secret_key = 'super secret key'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 SESSION_TYPE = 'redis'
 bcrypt = Bcrypt(app)
 
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'WriteFreeDB',
-    'host': '127.0.0.1',
-    'port': 27017
-}
-client = MongoClient(db_address)
-db = client['WriteFreeDB']
-credentials_collection = db['credentials']
-notes_collection = db['notes']
+jwt = JWTManager(app)
+blacklist = set()
 
-@app.before_request
-def session_management():
-    # make the session last indefinitely until it is cleared
-    session.permanent = True
+client = MongoClient('mongodb://localhost:27017/')
+credentials_collection = client['WriteFreeDB']['credentials']
+notes_collection = client['WriteFreeDB']['notes']
+application_collection = client['WriteFreeDB']['application']
 
-@app.route('/create-account_google', methods= ['POST', 'OPTIONS'])
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return jti in blacklist
+
+@app.route('/create-account-google', methods= ['POST', 'OPTIONS'])
 def create_google():
     email = request.args['email']
     google_id = request.args['google_id']
@@ -58,7 +63,8 @@ def create_google():
          "defaultNoteSettings": {},
         }
         document = control.new_account(credentials_collection, savedDocument)
-        return (document, 200)
+        return document, 200;
+
 
 # create account and store info into DB
 @app.route('/create-account', methods= ['POST', 'OPTIONS'])
@@ -90,48 +96,30 @@ def login():
     password = request.args['password']
     credentials = dbcalls.DB_find_one(credentials_collection, {'email': email})
     if (credentials):
-
-        # what does this if case do?
-        if (bcrypt.check_password_hash(credentials['password'], password.encode('utf-8'))):
-            arrayOfNotes = control.getArrayOfNotes(notes_collection, email)
         hashed_password = bcrypt.generate_password_hash(password)
         if (bcrypt.check_password_hash(hashed_password, password.encode('utf-8'))):
-            notes_and_credential = control.get_note_and_credential(notes_collection, credentials, email)
-            return notes_and_credential, 200
+            login_credential = control.get_credential(credentials, email)
+            return login_credential, 200
         return "Invalid Email or Password", 401
     return "Email Does Not Exist", 401
 
-@app.route('/login_google', methods= ['GET', 'OPTIONS'])
+@app.route('/login-google', methods= ['GET', 'OPTIONS'])
 def login_google():
     email = request.args['email']
     google_id = request.args['google_id']
     credentials = dbcalls.DB_find_one(credentials_collection, {'email': email})
     if (credentials):
         if (bcrypt.check_password_hash(credentials['password'], google_id.encode('utf-8'))):
-            notes_and_credential = control.get_note_and_credential(notes_collection, credentials, email)
-            return notes_and_credential, 200
+            login_credential = control.get_credential(credentials, email)
+            return login_credential, 200
         return "Invalid Email or Password", 401
     return "Email Does Not Exist", 401
 
 @app.route('/get-default-settings', methods= ['GET', 'OPTIONS'])
 def getDefaultSettings():
     email = request.args['email']
-    credentials = control.get_credential(credentials_collection, email)
-    return credentials, 200
-
-# verify username and password, returns account details and notes
-@app.route('/get-data', methods= ['GET', 'OPTIONS'])
-def getData():
-    email = request.args['email']
-    id = request.args['id']
-    credentials = dbcalls.DB_find_one(credentials_collection, {'email': email})
-    db_id = credentials['_id']
-    if (credentials):
-        if (id == str(db_id)):
-            notes_and_credential = control.get_note_and_credential(notes_collection, credentials, email)
-            return notes_and_credential, 200
-        return "Invalid Email or Password", 401
-    return "Email Does Not Exist", 401
+    cred_and_setting = control.get_default_setting(credentials_collection, application_collection, email)
+    return cred_and_setting, 200;
 
 @app.route('/get-notes', methods= ['GET', 'OPTIONS'])
 def getNotes():
@@ -143,7 +131,6 @@ def getNotes():
 def deleteNote():
     email = request.args['email']
     noteID = request.args['noteID']
-
     dbcalls.DB_delete_one(notes_collection, {'email': email, "_id": ObjectId(noteID)})
     notes = control.get_note(notes_collection, email)
     return notes, 200
@@ -161,6 +148,7 @@ def addNote():
         "noteSettings": defaultNoteSettings,
         "lastUpdated": datetime.datetime.fromtimestamp(time.time()).strftime('%c'),
         "category": None,
+        "noteColor": credentials['defaultNoteSettings']['noteColor']
     }
     notes = control.add_note(notes_collection, baseNewNote)
     return notes, 200
@@ -175,13 +163,21 @@ def saveNote():
 def updateDefaultSettings():
     form_data = json.loads(request.get_data())
     control.update_default_setting(credentials_collection, form_data)
-    return "Default setting updated", 200
+    return "Default Setting updated", 200
 
 @app.route ('/remove-tutorial', methods= ['POST', 'OPTIONS'])
 def removeTutorial():
     _id = ObjectId(request.args['_id'])
     control.disable_tutorial(credentials_collection, _id)
     return "Tutorial disabled", 200
+
+@app.route ('/change-note-color', methods= ['POST', 'OPTIONS'])
+def changeNoteColor():
+    noteID = ObjectId(request.args['noteID'])
+    noteColor = request.args['noteColor']
+    query = {'$set': {'noteColor': noteColor}}
+    dbcalls.DB_find_one_and_update(notes_collection, {'_id': noteID}, query)
+    return "Note Color Changed", 200
 
 @app.route ('/fetch-note/<note_id>', methods= ['GET', 'OPTIONS'])
 def fetchNote(note_id):
@@ -195,3 +191,55 @@ def renderPDF():
     noteID = request.args['noteID']
     response = control.render_PDF(notes_collection, noteID)
     return response
+
+#####JWT!!!######
+
+# verify username and password, returns account details and notes
+@app.route('/get-data', methods= ['GET'])
+@jwt_required
+def getData():
+    current_user = get_jwt_identity()
+    credentials = dbcalls.DB_find_one(credentials_collection, {'email': current_user})
+    if (credentials):
+        note_and_credential = control.get_note_and_credential(notes_collection, credentials, current_user)
+        return note_and_credential, 200
+    return "Invalid Email or Password", 401
+
+# verify username and password, returns account details and notes
+@app.route('/verify', methods= ['GET'])
+@jwt_required
+def verify():
+    current_user = get_jwt_identity()
+    bool = False
+    credentials = credentials_collection.find_one({'email': current_user})
+    if(credentials):
+        bool = True
+    return jsonify({"bool": bool}), 200;
+
+#Reissue a jwt token
+@app.route('/refresh', methods=['GET'])
+@jwt_refresh_token_required
+def refresh():
+    current_user = get_jwt_identity()
+    ret = {
+        'access_token': create_access_token(identity=current_user)
+    }
+    return jsonify(ret), 200
+
+
+# Endpoint for revoking the current users access token
+@app.route('/logout', methods=['GET'])
+@jwt_required
+def logout():
+    jti = get_raw_jwt()['jti']
+    blacklist.add(jti)
+    return jsonify({"msg": "Successfully logged out"}), 200
+
+
+# Endpoint for revoking the current users refresh token
+@app.route('/logout2', methods=['GET'])
+@jwt_refresh_token_required
+def logout2():
+    jti = get_raw_jwt()['jti']
+    blacklist.add(jti)
+    return jsonify({"msg": "Successfully logged2 out"}), 200
--- app/control.py
+++ app/control.py
@@ -1,10 +1,14 @@
 import MongoDBCalls as dbcalls
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from bson.objectid import ObjectId
 import datetime
 import time
 from draftjs_exporter.html import HTML
 import pdfkit
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token, create_refresh_token,
+    get_jwt_identity, jwt_refresh_token_required, get_raw_jwt
+)
 """
 This is the file containing the functions that handles the DB data before returning to the front-end
 """
@@ -16,8 +20,9 @@ def new_account(cred_db, savedDocument):
     dbcalls.DB_insert_one(cred_db, savedDocument)
     savedDocument["_id"] = str(savedDocument["_id"])
     del savedDocument['password']
-    document = jsonify({"notes": [], "credentials": savedDocument})
-    return document
+    access_token = create_access_token(identity=savedDocument["email"])
+    refresh_token = create_refresh_token(identity=savedDocument["email"])
+    return jsonify({"credentials": savedDocument, "access_token": access_token, "refresh_token": refresh_token})
 
 # input:
 #       DB: the database object for notes
@@ -30,14 +35,15 @@ def get_note_and_credential(note_db, credentials, email):
     return jsonify({"notes": arrayOfNotes, "credentials": credentials})
 
 # input:
-#       cred_db: the credential database object
+#       credential: the credential object
 #       email: user email for db retrieval
 # output: jsonified data for credentials
-def get_credential(cred_db, email):
-    credentials = dbcalls.DB_find_one(cred_db, {'email': email})
+def get_credential(credentials, email):
     credentials["_id"] = str(credentials["_id"])
     del credentials["password"]
-    return jsonify({"credentials": credentials})
+    access_token = create_access_token(identity=email)
+    refresh_token = create_refresh_token(identity=email)
+    return jsonify({"credentials": credentials, "access_token": access_token, "refresh_token": refresh_token})
 
 # input:
 #       note_db: the note database object
@@ -67,32 +73,27 @@ def save_note(note_db, form_data):
                   "lastUpdated": datetime.datetime.fromtimestamp(time.time()).strftime('%c')}
     dbcalls.DB_update_one(note_db, {"$set": new_values}, query)
 
-# input:
-#       note_db: the note database object
-#       form_data: new note data
-# output: None
-def save_note(note_db, form_data):
-    query = {"_id": ObjectId(form_data["noteID"])}
-    new_values = {"title": form_data['title'], "category": form_data['category'], "content": form_data['noteContent'],
-                  "lastUpdated": datetime.datetime.fromtimestamp(time.time()).strftime('%c')}
-    dbcalls.DB_update_one(note_db, {"$set": new_values}, query)
+def get_default_setting(cred_db, app_db, email):
+    credentials = dbcalls.DB_find_one(cred_db, {'email': email})
+    credentials["_id"] = str(credentials["_id"])
+    del credentials['password']
+    # WARNING: this assume that there is only one application setting within the database
+    applicationSettings = dbcalls.DB_find_one(cred_db, {})
+    applicationSettings["_id"] = str(applicationSettings["_id"])
+    return jsonify({"credentials": credentials, "applicationSettings": applicationSettings})
 
 # input:
 #       cred_db: credential data
 #       form_data: new note data
 # output: None
 def update_default_setting(cred_db, form_data):
-    _id = ObjectId(form_data["_id"])
+    email = form_data['email']
     noteColor = form_data['noteColor']
     fontName = form_data['fontName']
     fontSize = form_data['fontSize']
-    draftjs = {"blocks": [{"key": "9043t", "text": " ", "type": "unstyled", "depth": 0,
-                           "inlineStyleRanges": [{"offset": 0, "length": 1, "style": "fontsize-" + str(fontSize)},
-                                                 {"offset": 0, "length": 1, "style": "fontfamily-" + fontName}],
-                           "entityRanges": [], "data": {}}], "entityMap": {}}
-    query = {'$set': {'defaultNoteSettings': {'noteColor': noteColor, 'fontName': fontName, 'fontSize': fontSize,
-                                              'draftjsObj': draftjs}}}
-    dbcalls.DB_find_one_and_update(cred_db, {'_id': _id}, query)
+    draftjs = {"blocks":[{"key":"9043t","text":" ","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":0,"length":1,"style":"fontsize-" + str(fontSize)},{"offset":0,"length":1,"style":"fontfamily-" + fontName}],"entityRanges":[],"data":{}}],"entityMap":{}}
+    query = {'$set': {'defaultNoteSettings': {'noteColor': noteColor, 'fontName': fontName, 'fontSize': fontSize, 'draftjsObj': draftjs}}}
+    dbcalls.DB_find_one_and_update(cred_db, {'email': email}, query)
 
 # input:
 #       cred_db: the credential database object
@@ -100,8 +101,7 @@ def update_default_setting(cred_db, form_data):
 # output: None
 def disable_tutorial(cred_db, _id):
     query = {'$set': {'runTutorial': False}}
-    dbcalls.DB_find_one_and_update(cred_db, {'_id': _id}, query)
-
+    dbcalls.DB_find_one_and_update(cred_db, {'email': request.args['email']}, query)
 # input:
 #       cred_db: the credential database object
 #       value: value used for retrieval (dictionary)
