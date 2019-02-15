b04be6eef7ea51b8b353b7d1a3bd1be98b4ac129
Chris Ackerman <chrisackerman@Chriss-MBP.localdomain>
2019-02-15 10:38:43 -0500

    pushing latest changes to FE:

--- draftjs/src/dependency.js
+++ draftjs/src/dependency.js
@@ -1,6 +1,6 @@
 //this is the variable for controlling backend calls
 // local url
 var backendURL = "http://127.0.0.1:5000";
-// var backendURL = "ec2-18-191-185-79.us-east-2.compute.amazonaws.com:8080";
+// var backendURL = "https://www.writefreeapp.org";
 
 export {backendURL}
